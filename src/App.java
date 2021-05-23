068765488684a718c1625bf42313034b28030f5b
sami1793 <sami_17_94@hotmail.com>
2021-05-23 16:42:39 -0300

    Commitie a la Osa

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola sueñito");
         System.out.println("Hola Feluluuu");
-        System.out.println("Falta la felicidad");
+        System.out.println("Falta la Dulce");
+        System.out.println("Falta la Moshisha");
+
     }
 }
