c0102951c355e7fe52b6b3b1b18474f491c9a3b4
jonromero1010 <jromero.utn@gmail.com>
2021-05-23 16:39:08 -0300

    Editado commit2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola sueñito");
         System.out.println("Hola Feluluuu");
+        System.out.println("Falta la felicidad");
     }
 }
